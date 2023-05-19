88c8234a8da19c176274eacbfd463b3a0c653476
ubuntu-default <lsb>
2023-05-19 15:54:12 +0000

    lab20_sol

--- lab19_container/toy_sol/ui/gui.c
+++ lab19_container/toy_sol/ui/gui.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <errno.h>
-#include <stdio.h>
-#include <unistd.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
-int create_web_server()
+int create_gui()
 {
     pid_t systemPid;
 
-    printf("여기서 Web Server 프로세스를 생성합니다.\n");
+    printf("여기서 GUI 프로세스를 생성합니다.\n");
 
+    sleep(3);
+    /* fork + exec 를 이용하세요 */
+    /* exec으로 google-chrome-stable을 실행 하세요. */
 
-    // fork를 clone + 새로운 namespace로 생성하세요.
     switch (systemPid = fork()) {
     case -1:
         printf("fork failed\n");
     case 0:
-        if (execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8282", (char *) NULL)) {
+        if (execl("/usr/bin/google-chrome-stable", "google-chrome-stable", "http://localhost:8282", NULL)) {
             printf("execfailed\n");
         }
         break;
